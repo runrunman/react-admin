21bb35d04b867e50e805593a1627435911adaf7d
zhilin <1103402143@qq.com>
2019-10-23 08:56:40 +0800

    l

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,14 @@
-const {override, fixBabelImports} = require('customize-cra');
+const {override, fixBabelImports,addLessLoader,addDecoratorsLegacy} = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css',
+    style: true,
   }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {'@primary-color': '#1DA57A'},
+  }),
+  addDecoratorsLegacy(),
 );
